f9897a1c6e7af40553da350aa524d2540d55ac48
efloresjc <everardo.flores@jumpcloud.com>
2023-08-24 23:57:22 -0600

    added reading any filename with .json extension

--- localModule.py
+++ localModule.py
@@ -3,8 +3,8 @@ from base64 import b64encode
 from nacl import encoding, public
 
 
-personalAccessToken = os.environ['GIT_PAT']
-ORG_NAME = os.environ['GIT_OWNER']
+personalAccessToken = os.environ['POC_GIT_PAT']
+ORG_NAME = os.environ['POC_GIT_OWNER']
 
 ################################-Initializing global vars-##############################################
 token = "Bearer " + personalAccessToken
@@ -12,11 +12,22 @@ header = {
     "X-GitHub-Api-Version": "2022-11-28",
     "Authorization": token
 }
+
 body = {
         "encrypted_value": "",
         "key_id": ""
     }
 
+orgVarBody = {
+        "name": "",
+        "value": "",
+        "visibility": "all"
+    }
+
+repoVarBody = {
+        "name":"workers",
+        "value":"2"
+    }
 #################################-Encrypt Secret Value Function-#################################
 def encrypt(repoPublicKey:str, secret_value: str) -> str:
   """Encrypt a Unicode string using the public key."""
@@ -42,10 +53,84 @@ def getRepoPublicKey(repoName:str)->dict:
         print("Error: While generating KeyId and publicKey")
         print("Error: "+response.text)
     return encryptionResorces
-        
 
-#################################-Upsert ORG/REPO Secrets Function-#################################
-def upsertSecretOrVariable(repoName: str, secretValue: str, secretName:str):
+
+###############################-Get ORG Public Key Funtion-####################################
+def getOrgPublicKey()->dict:
+    url= "https://api.github.com/orgs/"+ORG_NAME+"/actions/secrets/public-key"
+    response = requests.get(url, headers = header)
+    encryptionResorces = {
+        "Key_id": "",
+        "public_key": ""
+    }
+    if response.status_code == 200:
+        response =json.loads(response.text)
+        encryptionResorces["key_id"] = response["key_id"]
+        encryptionResorces["public_key"] = response["key"]
+        print("KeyId and PublicKey generated successfully")
+    else:
+        print("Error: While generating KeyId and publicKey")
+        print("Error: "+response.text)
+    return encryptionResorces
+
+#################################-Upsert ORG Secrets Function-#################################
+def upsertOrgSecret(orgSecretName:str, orgSecretValue:str):
+    encryptionResources = getOrgPublicKey()
+    secretValue = encrypt(encryptionResources["public_key"],orgSecretValue)
+    body = {
+        "encrypted_value": "",
+        "key_id": "",
+        "visibility": "all"
+    }
+    body["encrypted_value"] = secretValue
+    body["key_id"] = encryptionResources["key_id"]
+    url = "https://api.github.com/orgs/"+ORG_NAME+"/actions/secrets/"+orgSecretName
+    response = requests.request("PUT",url,headers=header,data=json.dumps(body))
+    
+    if response.status_code == 201:
+        print("Secret "+orgSecretName+" created successfully")
+    else:
+        if response.status_code == 204:
+            print("Secret "+orgSecretName+" updated successfully")
+        else:
+            print("Error: "+response.text)
+
+#################################-Create ORG Variables Function-#################################
+def createOrgVariable(varName:str, varValue:str)->int:
+    url = "https://api.github.com/orgs/"+ORG_NAME+"/actions/variables"
+    responseCode = 0
+    orgVarBody['name'] = varName
+    orgVarBody['value'] = varValue
+    response = requests.request("POST",url,headers=header,data=json.dumps(orgVarBody))
+    responseCode = response.status_code
+    if response.status_code == 201:
+        print("Variable "+varName+" created successfully")
+    else:
+        print("Error: "+response.text)
+    return responseCode
+
+#################################-Update ORG Variables Function-#################################
+def updateOrgVariable(varName:str, varValue:str)->int:
+    url = "https://api.github.com/orgs/"+ORG_NAME+"/actions/variables/"+varName
+    responseCode = 0
+    orgVarBody['name'] = varName
+    orgVarBody['value'] = varValue
+    response = requests.request("PATCH",url,headers=header,data=json.dumps(orgVarBody))
+    responseCode = response.status_code
+    if response.status_code == 204:
+        print("Variable "+varName+" updated successfully")
+    else:
+        print("Error: "+response.text)
+    return responseCode
+
+#################################-Upsert ORG Variables Function-#################################
+def upsertOrgVariable(varName:str, varValue:str)->int:
+    response = createOrgVariable(varName, varValue)
+    if response != 201:
+        updateOrgVariable(varName, varValue)
+
+#################################-Upsert REPO Secrets Function-#################################
+def upsertRepoSecret(repoName: str, secretName:str, secretValue: str):
     
     encryptionResources = getRepoPublicKey(repoName)
     secretValue = encrypt(encryptionResources["public_key"],secretValue)
@@ -63,8 +148,40 @@ def upsertSecretOrVariable(repoName: str, secretValue: str, secretName:str):
         else:
             print("Error: "+response.text)
 
+#################################-Create REPO Variables Function-#################################
+def createRepoVariable(repoName:str, varName:str, varValue:str)-> int:
+    url = "https://api.github.com/repos/"+ORG_NAME+"/"+repoName+"/actions/variables"
+    repoVarBody['name'] = varName
+    repoVarBody['value'] = varValue
+    response = requests.request("POST",url,headers=header,data=json.dumps(repoVarBody))
+    responseCode = response.status_code
+    if response.status_code == 201:
+        print("Variable "+varName+" created successfully")
+    else:
+        print("Error: "+response.text)
+    return responseCode
+
+#################################-Update REPO Variables Function-#################################
+def updateRepoVariable(repoName:str, varName:str, varValue:str)-> int:
+    url = "https://api.github.com/repos/"+ORG_NAME+"/"+repoName+"/actions/variables/"+varName
+    repoVarBody['name'] = varName
+    repoVarBody['value'] = varValue
+    response = requests.request("PATCH",url,headers=header,data=json.dumps(repoVarBody))
+    responseCode = response.status_code
+    if response.status_code == 204:
+        print("Variable "+varName+" updated successfully")
+    else:
+        print("Error: "+response.text)
+    return responseCode
+
+#################################-Upsert REPO Variables Function-#################################
+def upsertRepoVariable(repoName:str, varName:str, varValue:str):
+    response = createRepoVariable(repoName, varName, varValue)
+    if response != 201:
+        updateRepoVariable(repoName, varName, varValue)
+        
 #################################-Trigger Workflow Function-#################################
-def triggerWorkflow(repoName: str):
+def triggerWorkflow(repoName: str,env:str):
     url = "https://api.github.com/repos/"+ORG_NAME+"/"+repoName+"/actions/workflows"
     getResp = requests.get(url, headers = header)
     if getResp.status_code == 200:
@@ -73,8 +190,10 @@ def triggerWorkflow(repoName: str):
         wfId=getResp['workflows'][0]['id']
         url = "https://api.github.com/repos/"+ORG_NAME+"/"+repoName+"/actions/workflows/"+str(wfId)+"/dispatches"
         request = {
-            "ref":"dev"
+            "ref": env
         }
         response = requests.request("POST",url,headers=header,data=json.dumps(request))
         if response.status_code == 204 :
             print("====> Status: Wf Triggered Successfully")
+        else: print("Error while triggering the workflow from repo: "+ repoName)
+    else: print("Error while getting workflow ID from repo: "+ repoName)
--- main.py
+++ main.py
@@ -1,26 +1,61 @@
-import json
+import json,os,glob
 import localModule as lm
 
-rpath = "/home/runner"
+rpath = os.environ['GITHUB_ROOT_DIR']
 #rpath = os.path.dirname(__file__) #Used for local testing
-repoNames = set()
+repoNames = set() #Using set to store unic values
+
+#Getting the .json file name
+files = glob.glob("*.json")
+
+#Getting the environment from the file name
+env = files[0].split("_")
+env = env[1].split(".")
+env = env[0]
+print("\n====>Executing on [" + env + "] environment\n")
 
 #Reading decrypted file
-with open(rpath + "/payload.json") as f:
+with open(rpath + "/"+files[0]) as f:
     payload = json.load(f)
 
+#Upsert git actions org secrets
+if len(payload['orgGithubActions']['secretList']) > 0:
+    print('****************ORG Secrets*********************')
+    for orgSecret in payload['orgGithubActions']['secretList']:
+        #lm.upsertOrgSecret(orgSecret['name'], orgSecret['value'])
+        print("Secret Name: " + orgSecret['name'])
 
-#Upsert git actions repo secrets
-for repoData in payload['repoSecrets']:
-    print('*************************************')
-    print(repoData['repoName'])
-    for repoSecrets in repoData['repoSecret']:
-        lm.upsertSecretOrVariable(repoData['repoName'],repoSecrets['value'],repoSecrets['name'])
+#Upsert git actions org variables
+if len(payload['orgGithubActions']['variableList']) > 0:
+    print('***************ORG Variables**********************')
+    for orgVariable in payload['orgGithubActions']['variableList']:
+        #lm.upsertOrgVariable(orgVariable['name'], orgVariable['value'])
+        print("Variable Name: " + orgVariable['name'])
+
+if len(payload['orgGithubActions']['reposToTrigger']) > 0:
+    print("[ORG level]-Triggering workflows from below repos:")
+    for repo in payload['orgGithubActions']['reposToTrigger']:
+        print("Repo Name: " + repo)
+        lm.triggerWorkflow(repo, env)
+          
+#Upsert git actions repo secrets/variables
+if len(payload['repoGithubActions']) > 0:
+    print('*****************REPO Secrets and Variables********************')
+    for repoData in payload['repoGithubActions']:
+        print("====>REPO Name: " + repoData['repoName'])
         repoNames.add(repoData['repoName'])
-
+        for secret in repoData['secretList']:
+            print("Secret Name: " + secret['name'])
+            lm.upsertRepoSecret(repoData['repoName'], secret['name'], secret['value'])
+        for variable in repoData['variableList']:
+            print("Variable Name: " + variable['name'])
+            lm.upsertRepoVariable(repoData['repoName'], variable['name'], variable['value'])
+            
+        
 #Triggering git actions workflows
-for repo in repoNames:
-    lm.triggerWorkflow(repo)
-
-    
+if len(repoNames) > 0:
+    print("[REPO level]-Triggering workflows from below repos:")
+    for repo in repoNames:
+        print("Repo Name: " + repo)
+        lm.triggerWorkflow(repo, env)
         
